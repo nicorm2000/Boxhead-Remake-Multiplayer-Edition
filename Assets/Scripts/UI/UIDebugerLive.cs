28abdc4f88760653701f49c7691789e46a7af8e1
juliMAB <julian6aguirre@hotmail.com>
2023-05-16 12:19:51 -0300

    Update scene testing life.

--- Assets/Scripts/UI/UIDebugerLive.cs
+++ Assets/Scripts/UI/UIDebugerLive.cs
@@ -5,11 +5,17 @@ using UnityEngine.UI;
 public class UIDebugerLive : MonoBehaviour
 {
     [SerializeField] private Button damagePlayer;
+    [SerializeField] private Button damagePlayer2;
+    [SerializeField] private Button damagePlayer3;
     [SerializeField] private Slider playerLive;
     [SerializeField] private Player player;
     private void Start()
     {
         damagePlayer.onClick.AddListener(() => { player.TakeDamage(1); });
-        player.InitCanvasLive((float a) => playerLive.value = a);
+        damagePlayer2.onClick.AddListener(() => { player.TakeDamage(10); });
+        damagePlayer3.onClick.AddListener(() => { player.TakeDamage(20); });
+
+
+        player.InitCanvasLive((float a) => playerLive.value = a); //this line need to use in new scripts whe life load.
     }
 }
